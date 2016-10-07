1eb494e0895ef7f7965af616f22c68c5bba6fab5
pseudobyte <pseudobyte@gmail.com>
2016-10-06 21:42:25 -0600

    moved language data to own file

---
+++ src/js/data/language_data.js
@@ -0,0 +1,8 @@
+import Lang from '../language.js';
+
+Lang.add_language('en', {parent:null, entries:{
+  'res_smoke': 'Smoke',
+  'res_meat': 'Meat',
+  'res_metal': 'Metal',
+  'res_plastic': 'Plastic'
+}});
--- src/js/learning.js
+++ src/js/learning.js
@@ -1,14 +1,7 @@
+import './data/language_data.js'
+
 import Vue from 'vue';
-import Lang from './language.js';
 import Main from '../vue/main.vue';
-
-Lang.add_language('en', {parent:null, entries:{
-  'res_smoke': 'Smoke',
-  'res_meat': 'Meat',
-  'res_metal': 'Metal',
-  'res_plastic': 'Plastic'
-}});
-
 var View = new Vue(Main);
 
 document.addEventListener('DOMContentLoaded', function() {
